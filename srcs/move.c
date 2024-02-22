3042e52831aef4080271b2da07e30a14d9900ac3
Maelle POIRIER <mapoirie@paul-f5Ar3s1.clusters.42paris.fr>
2024-02-22 17:49:45 +0100

    22.02 17h40

--- srcs/move.c
+++ srcs/move.c
@@ -23,20 +23,29 @@ void	move_up(t_game *game)// W key
 
 void	move_down(t_game *game)// S key
 {
-	// char	charX;
-	// char	charY;
+	char	charX;
+	char	charY;
 
-	// charX = game->map[][];
-	// charY = game->map[][];
+	charX = game->map[(int)(game->posY)][(int)(game->posX - game->dirX * (game->moveSpeed))];
+	charY = game->map[(int)(game->posY + game->dirY * (game->moveSpeed))][(int)(game->posX)];
 
-	game->posX -= game->dirX * game->moveSpeed;
-	game->posY -= game->dirY * game->moveSpeed;
+	if (charX != '1')
+		game->posX -= game->dirX * game->moveSpeed;
+	if (charY != '1')
+		game->posY -= game->dirY * game->moveSpeed;
 }
 
 void	move_left(t_game *game)// A key
 {
+	// char	charX;
+	// char	charY;
 	double	facing_direction;
+
 	facing_direction = calculate_player_direction(game);
+
+	// charX = game->map[(int)game->posY][(int)(game->posX + cos(facing_direction + M_PI_2) * game->moveSpeed)];
+	// charY = game->map[(int)(game->posY + sin(facing_direction + M_PI_2) * game->moveSpeed)][(int)game->posX];
+
 	game->posX += cos(facing_direction + M_PI_2) * game->moveSpeed;
 	game->posY += sin(facing_direction + M_PI_2) * game->moveSpeed;
 
@@ -46,8 +55,15 @@ void	move_left(t_game *game)// A key
 
 void	move_right(t_game *game)// D key
 {
+	// char	charX;
+	// char	charY;
 	double	facing_direction;
+
 	facing_direction = calculate_player_direction(game);
+
+	// charX = game->map[(int)game->posY][(int)(game->posX + cos(facing_direction - M_PI_2) * game->moveSpeed)];
+	// charY = game->map[(int)(game->posY + sin(facing_direction - M_PI_2) * game->moveSpeed)][(int)game->posX];
+
 	game->posX += cos(facing_direction - M_PI_2) * game->moveSpeed;
 	game->posY += sin(facing_direction - M_PI_2) * game->moveSpeed;
 
