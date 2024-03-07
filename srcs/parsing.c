c9f2b5f6cc1c8390e371f2b05264e132ab2ae2c0
Claire CARNOT <ccarnot@paul-f5Ar4s1.clusters.42paris.fr>
2024-03-07 10:36:54 +0100

    derniere norme

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -12,57 +12,12 @@ int	tab_size(char **tab)
 
 int	parse_content(t_game *game)
 {
-	/****************************************
-	int i = 0;
-	while (game->type[i])
-	{
-		dprintf(2, "%s", game->type[i]);
-		i++;
-	}
-	dprintf(2, "\n");
-	**************************************/
 	if (redef_types(game) == -1)
 		return (1);
-	/**************************************
-	i = 0;
-	while (game->type[i])
-	{
-		dprintf(2, "%s\n", game->type[i]);
-		i++;
-	}
-	**************************************/
 	if (check_err_types(game) == -1)
 		return (1);
-	/**************************************
-	int j = 0;
-	while (game->map[j])
-	{
-		int y = 0;
-		while (game->map[j][y])
-		{
-			if (game->map[j][y] == ' ')
-				dprintf(2, "x");
-			else
-				dprintf(2, "%c", game->map[j][y]);
-			y++;
-		}
-		j++;
-	}
-	dprintf(2, "\n");
-	dprintf(2, "\n");
-	**************************************/
 	if (redef_map(game) == -1)
 		return (1);
-	/**************************************
-	int j = 0;
-	while (game->map[j])
-	{
-		dprintf(2, "%s", game->map[j]);
-		j++;
-	}
-	**************************************/
-//	dprintf(2, "\ngame->rows = %d\n", game->rows);
-//	dprintf(2, "game->cols = %d\n", game->cols);
 	if (check_err_map(game) == -1)
 		return (1);
 	if (BONUS)
--- srcs_bonus/parsing.c
+++ srcs_bonus/parsing.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccarnot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 10:35:40 by ccarnot           #+#    #+#             */
+/*   Updated: 2024/03/07 10:35:41 by ccarnot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/cub3D_bonus.h" 
 
 int	tab_size(char **tab)
@@ -12,57 +24,12 @@ int	tab_size(char **tab)
 
 int	parse_content(t_game *game)
 {
-	/****************************************
-	int i = 0;
-	while (game->type[i])
-	{
-		dprintf(2, "%s", game->type[i]);
-		i++;
-	}
-	dprintf(2, "\n");
-	**************************************/
 	if (redef_types(game) == -1)
 		return (1);
-	/**************************************
-	i = 0;
-	while (game->type[i])
-	{
-		dprintf(2, "%s\n", game->type[i]);
-		i++;
-	}
-	**************************************/
 	if (check_err_types(game) == -1)
 		return (1);
-	/**************************************
-	int j = 0;
-	while (game->map[j])
-	{
-		int y = 0;
-		while (game->map[j][y])
-		{
-			if (game->map[j][y] == ' ')
-				dprintf(2, "x");
-			else
-				dprintf(2, "%c", game->map[j][y]);
-			y++;
-		}
-		j++;
-	}
-	dprintf(2, "\n");
-	dprintf(2, "\n");
-	**************************************/
 	if (redef_map(game) == -1)
 		return (1);
-	/**************************************
-	int j = 0;
-	while (game->map[j])
-	{
-		dprintf(2, "%s", game->map[j]);
-		j++;
-	}
-	**************************************/
-//	dprintf(2, "\ngame->rows = %d\n", game->rows);
-//	dprintf(2, "game->cols = %d\n", game->cols);
 	if (check_err_map(game) == -1)
 		return (1);
 	if (BONUS)
